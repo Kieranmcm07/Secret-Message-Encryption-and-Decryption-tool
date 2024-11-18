2ac3152db28e4b1bdd5041a1cfd508ef46d21a7f
Nokky07 <118632291+Nokky07@users.noreply.github.com>
2024-11-18 11:35:14 +0000

    Creating main screen

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+import os
+
+def main_screen():
+    
+    screen=Tk()
+    screen.geometry("375x398")
+    
+    screen.title("PctApp")
+
+    screen.mainloop()
+main_screen()
