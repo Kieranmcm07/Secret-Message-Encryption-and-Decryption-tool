9c376eba16ca3a6a4ee8b45327d83a12a8d5ba89
Nokky07 <118632291+Nokky07@users.noreply.github.com>
2024-11-18 12:54:00 +0000

    Formatted

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import base64
 import os
 
 # Function to handle decryption
+
+
 def decryption_key():
     # Get the decryption key from the input field
     key = code.get()
@@ -26,18 +28,24 @@ def decryption_key():
         decryption_key = base64_bytes.decode("ascii")
 
         # Display the decrypted message in the new window
-        Label(screen2, text="DECRYPTION RESULTS", font="Arial", fg="white", bg="#00bd56").place(x=10, y=0)
-        text2 = Text(screen2, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+        Label(screen2, text="DECRYPTION RESULTS", font="Arial",
+              fg="white", bg="#00bd56").place(x=10, y=0)
+        text2 = Text(screen2, font="Rpbote 10", bg="white",
+                     relief=GROOVE, wrap=WORD, bd=0)
         text2.place(x=10, y=40, width=380, height=150)
         text2.insert(END, decryption_key)
 
     # Handle incorrect or empty key
     elif key == "":
-        messagebox.showerror("Encryption and Decryption Tool", "Input Password")
+        messagebox.showerror(
+            "Encryption and Decryption Tool", "Input Password")
     elif key != "1234":
-        messagebox.showerror("Encryption and Decryption Tool", "Invalid Password")
+        messagebox.showerror(
+            "Encryption and Decryption Tool", "Invalid Password")
 
 # Function to handle encryption
+
+
 def encryption_key():
     # Get the encryption key from the input field
     key = code.get()
@@ -59,18 +67,24 @@ def encryption_key():
         encryption_key = base64_bytes.decode("ascii")
 
         # Display the encrypted message in the new window
-        Label(screen1, text="ENCRYPTION RESULTS", font="Arial", fg="white", bg="#ed3833").place(x=10, y=0)
-        text2 = Text(screen1, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+        Label(screen1, text="ENCRYPTION RESULTS", font="Arial",
+              fg="white", bg="#ed3833").place(x=10, y=0)
+        text2 = Text(screen1, font="Rpbote 10", bg="white",
+                     relief=GROOVE, wrap=WORD, bd=0)
         text2.place(x=10, y=40, width=380, height=150)
         text2.insert(END, encryption_key)
 
     # Handle incorrect or empty key
     elif key == "":
-        messagebox.showerror("Encryption and Decryption Tool", "Please Enter a Password")
+        messagebox.showerror(
+            "Encryption and Decryption Tool", "Please Enter a Password")
     elif key != "1234":
-        messagebox.showerror("Encryption and Decryption Tool", "Incorrect Password")
+        messagebox.showerror(
+            "Encryption and Decryption Tool", "Incorrect Password")
 
 # Function to create the main display
+
+
 def main_display():
     global screen
     global code
@@ -93,18 +107,25 @@ def main_display():
         text1.delete(1.0, END)
 
     # Create the input fields and buttons
-    Label(text="Enter text for Encryption and Decryption", fg="black", font=("calbri", 13)).place(x=10, y=10)
+    Label(text="Enter text for Encryption and Decryption",
+          fg="black", font=("calbri", 13)).place(x=10, y=10)
     text1 = Text(font="Arial 20", bg="white", relief=GROOVE, wrap=WORD, bd=0)
     text1.place(x=10, y=50, width=355, height=100)
 
-    Label(text="Enter Secret Key for Encryption and Decryption", fg="black", font=("Arial", 13)).place(x=10, y=170)
+    Label(text="Enter Secret Key for Encryption and Decryption",
+          fg="black", font=("Arial", 13)).place(x=10, y=170)
 
     code = StringVar()
-    Entry(textvariable=code, width=19, bd=0, font=("Arial", 25), show="*").place(x=10, y=200)
+    Entry(textvariable=code, width=19, bd=0, font=(
+        "Arial", 25), show="*").place(x=10, y=200)
 
-    Button(text="ENCRYPT", height="2", width=23, bg="#ed3833", fg="white", bd=0, command=encryption_key).place(x=10, y=250)
-    Button(text="DECRYPT", height="2", width=23, bg="#00bd56", fg="white", bd=0, command=decryption_key).place(x=200, y=250)
-    Button(text="RESET", height="2", width=50, bg="#1089ff", fg="white", bd=0, command=reset).place(x=10, y=300)
+    Button(text="ENCRYPT", height="2", width=23, bg="#ed3833",
+           fg="white", bd=0, command=encryption_key).place(x=10, y=250)
+    Button(text="DECRYPT", height="2", width=23, bg="#00bd56",
+           fg="white", bd=0, command=decryption_key).place(x=200, y=250)
+    Button(text="RESET", height="2", width=50, bg="#1089ff",
+           fg="white", bd=0, command=reset).place(x=10, y=300)
+
 
 main_display()
 screen.mainloop()
