f0978d41b7acb7bdc42b28b6faeacade0ea21027
Nokky07 <118632291+Nokky07@users.noreply.github.com>
2024-11-18 12:08:05 +0000

    Made encrypt

--- main.py
+++ main.py
@@ -4,7 +4,35 @@ import base64
 import os
 
 
+def decrypt():
+    print("")
+
+
+def encrypt():
+    password = code.get()
+
+    if password == "1234":
+        screen1 = Toplevel(screen)
+        screen1.title("Encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+
+        message = text1.get(1.0, END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt = base64_bytes.decode("ascii")
+
+        Label(screen1,text="ENCRYPT",font="arial",fg="white",bg="#ed3833").place(x=10,y=0)
+        text2=Text(screen1,font="Rpbote 10", bg="white",relief=GROOVE,wrap=WORD,bd=0)
+        text2.place(x=10,y=40,width=380,height=150)
+
+        text2.insert(END,encrypt)
+
 def main_screen():
+
+    global screen
+    global code
+    global text1
 
     screen = Tk()
     screen.geometry("375x398")
@@ -18,7 +46,6 @@ def main_screen():
     def reset():
         code.set("")
         text1.delete(1.0, END)
-        
 
     Label(text="Enter text for encryption and decryption",
           fg="black", font=("calbri", 13)).place(x=10, y=10)
@@ -33,11 +60,11 @@ def main_screen():
           font=("arial", 25), show="*").place(x=10, y=200)
 
     Button(text="ENCRYPT", height="2", width=23,
-           bg="#ed3833", fg="white", bd=0).place(x=10, y=250)
+           bg="#ed3833", fg="white", bd=0, command=encrypt).place(x=10, y=250)
     Button(text="DECRYPT", height="2", width=23,
-           bg="#00bd56", fg="white", bd=0).place(x=200, y=250)
+           bg="#00bd56", fg="white", bd=0, command=decrypt).place(x=200, y=250)
     Button(text="RESET", height="2", width=50, bg="#1089ff",
-           fg="white", bd=0,command=reset).place(x=10, y=300)
+           fg="white", bd=0, command=reset).place(x=10, y=300)
     screen.mainloop()
 
 
